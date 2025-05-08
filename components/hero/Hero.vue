c8237f2362f1903e59c925e273e5170137972765
Binh Minh <bmmit.work@gmail.com>
2025-05-08 14:42:21 +0700

    Initial commit

---
+++ components/hero/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="@container container min-h-screen flex mx-auto select-none">
+    <div class="my-auto px-4">
+      <h1 class="text-6xl mb-8 text-primary font-semibold">
+        EduPass
+      </h1>
+
+      <h2 class="font-semibold text-2xl @sm:text-4xl mb-4">
+        The ultimate solution for
+        <br>
+        <b class="text-primary">digitized certifications</b>
+      </h2>
+
+      <h3 class="text-muted-foreground italic mb-6">
+        Seamlessly verify and share your credentials with EduPass' cutting-edge digital solution.
+      </h3>
+
+      <div class="flex">
+        <Button>Get started</Button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+  ],
+  shadcn: {
+    prefix: '',
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  typescript: {
+    typeCheck: true,
+  },
+  css: ['~/assets/css/tailwind.css'],
+})
